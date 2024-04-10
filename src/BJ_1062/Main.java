63e1322d45bdd64d374ae5d1e0a9e10587b546e5
vattis <superhsk00@gmail.com>
2024-04-10 14:03:40 +0900

    .

--- src/BJ_1062/Main.java
+++ src/BJ_1062/Main.java
@@ -3,31 +3,61 @@ package BJ_1062;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import static java.lang.Math.pow;
 
 class Solution {
+    int N, K;
     String[][] words;
     String[] alpha = new String[26];
-
-    public Solution(String[][] words_){
+    Set<String> alphaSet = new HashSet<>();
+    public Solution(String[][] words_, int n, int k){
         words = words_;
         for(int i = 0; i<26; i++){
             alpha[i] = (char)('a'+i)+"";
         }
+        N = n;
+        K = k;
+        alphaSet.add("a");
+        alphaSet.add("c");
+        alphaSet.add("i");
+        alphaSet.add("n");
+        alphaSet.add("t");
     }
-    public int solution(int N, int K) {
+    public int recur(int start, int cnt){
+        int MAX = -1;
+        if(cnt < K){
+            for(int i = start; i < 26; i++){
+                if(!alphaSet.contains(alpha[i])){
+                    alphaSet.add(alpha[i]);
+                    int t = recur(i+1, cnt+1);
+                    if(MAX <  t){MAX = t;}
+                    alphaSet.remove(alpha[i]);
+                }
+            }
+        }
+        else if(cnt == K){
+            return cntAvailableWords(alphaSet);
+        }
+        return MAX;
+    }
+    public int cntAvailableWords(Set<String> alphaSet){
+        int ans = 0;
         for(int i = 0; i < N; i++){
-
+            if(words[i].length == 0){ans++;}
+            for(int j = 0; j < words[i].length; j++){
+                if(alphaSet.contains(words[i][j])){
+                    ans++;
+                    break;
+                }
+                j++;
+            }
         }
-        return 1;
-    }
-    public int recur(int start, int cnt, int K){
-        for(int i = start; i < 25; i++){
-            recur(start, cnt, K);
-        }
+        return ans;
     }
 }
 
@@ -44,7 +74,11 @@ public class Main {
             str = str.substring(4, str.length()-4);
             words[i] = str.split("");
         }
-        Solution solution = new Solution(words);
-        solution.solution(N, K);
+        if(K < 5){
+            System.out.println(0);
+            return;
+        }
+        Solution solution = new Solution(words, N, K);
+        System.out.println(solution.recur(0, 5));
     }
 }
