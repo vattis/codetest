6c7bfe39781455e9fa674fb1aaa6df241e9f8c56
vattis <superhsk00@gmail.com>
2024-05-10 16:29:50 +0900

    .

--- src/BJ_1005/Main.java
+++ src/BJ_1005/Main.java
@@ -1,11 +1,12 @@
 package BJ_1005;
 
+//위상정렬 알고리즘 필요
+//다른것보다 서로 연결된 노드 찾는게 어려웠음
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] ars) throws IOException {
@@ -16,59 +17,60 @@ public class Main {
         T = Integer.parseInt(br.readLine());  // T입력
 
         for (int t = 0; t < T; t++) {
+            Queue<Integer> queue = new LinkedList<>();
             int[] craftTime = new int[1001];
-            boolean[][] craftGraph = new boolean[1001][];
+            int[] minTime = new int[1001];
+            Arrays.fill(minTime, 0);
+            ArrayList<Queue<Integer>> craftGraph = new ArrayList<>();
+            ArrayList<Queue<Integer>> craftGraphReverse = new ArrayList<>();
+
             for (int i = 0; i <= 1000; i++) {
-                craftGraph[i] = new boolean[1001];
+                craftGraph.add(new LinkedList<>());
+                craftGraphReverse.add(new LinkedList<>());
+
             }
             StringTokenizer st = new StringTokenizer(br.readLine());  //  N, K 입력
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine()); //건물당 건설 시간
-            int k = 0;
+            int k = 1;
             while (st.hasMoreTokens()) {
                 craftTime[k] = Integer.parseInt(st.nextToken());
                 k++;
             }
             for (int i = 0; i < K; i++) {  //건물 건설 조건 입력
-                st = new StringTokenizer(new StringBuilder(br.readLine()).reverse().toString());
+                st = new StringTokenizer(br.readLine());
                 int X = Integer.parseInt(st.nextToken());
                 int Y = Integer.parseInt(st.nextToken());
-                craftGraph[Y][X] = true;
+                craftGraph.get(Y).add(X);
+                craftGraphReverse.get(X).add(Y);
             }
-            target = Integer.parseInt(br.readLine()); //목표 건물 입력
-
-            Solution sol = new Solution(craftTime, craftGraph);
-            System.out.println(sol.solution(target));
+            target = Integer.parseInt(br.readLine()); // 목표 건물 입력
+            for(int i = 1; i <= N; i++){
+                if(craftGraph.get(i).isEmpty())
+                    queue.add(i);
+            }
+            while(!queue.isEmpty()){
+                int curNode = queue.poll();
+                while(!craftGraphReverse.get(curNode).isEmpty()){
+                    int nextNode = craftGraphReverse.get(curNode).poll();
+                    craftGraph.get(nextNode).remove(curNode);
+                    if(minTime[curNode] + craftTime[curNode] > minTime[nextNode]){
+                        minTime[nextNode] = minTime[curNode] + craftTime[curNode];
+                    }
+                    if(craftGraph.get(nextNode).isEmpty()){
+                        queue.add(nextNode);
+                    }
+                }
+            }
+            System.out.println(minTime[target] + craftTime[target]);
         }
     }
 }
 class Solution{
-    ArrayList<Integer>[] craftGraph;
-    ArrayList<Integer> craftTime;
+    int[] minTime = new int[1001];
+    ArrayList<Set<Integer>> craftGraph = new ArrayList<>();
 
-    public Solution(ArrayList<Integer> craftTime_, ArrayList<Integer>[] craftGraph_){
-        craftTime = craftTime_;
-        craftGraph = craftGraph_;
-    }
-    public int solution(int target){
-        return recur(target);
-    }
-    public int recur(int building){
-        int max = 0;
-        int buildTime = craftTime.get(building);
-
-        for(int postBuilding : craftGraph[building]){
-            int pt = recur(postBuilding);
-            if(max < pt){
-                max = pt;
-            }
-        }
-        buildTime += max;
-        craftTime.set(building, buildTime);
-        craftGraph[building].clear();
-        return buildTime;
-    }
 }
 
 
