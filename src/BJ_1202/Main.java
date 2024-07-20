45790f8c472518c74826be47880b16131ae3e3dc
vattis <superhsk00@gmail.com>
2024-07-21 04:10:33 +0900

    .

--- src/BJ_1202/Main.java
+++ src/BJ_1202/Main.java
@@ -1,23 +1,21 @@
 package BJ_1202;
-
+//지속적으로 sort를 사용해서 값을 정리해야하는 경우 priority queue를 사용하자
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] ars) throws IOException {
-
+        Solution solution = new Solution();
     }
 }
 class Solution {
     int N, K;
-    PriorityQueue<Pair> jewels = new PriorityQueue<>(Collections.reverseOrder());
-    PriorityQueue<Integer> bag = new PriorityQueue<>();
-
+    long ans = 0;
+    PriorityQueue<Integer> jewels = new PriorityQueue<>(Collections.reverseOrder());
+    List<Integer> bags = new ArrayList<>();
+    List<Pair> js = new ArrayList<>();
     public Solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,26 +24,37 @@ class Solution {
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            jewels.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            js.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
         for(int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            bag.add(Integer.parseInt(st.nextToken()));
+            bags.add(Integer.parseInt(br.readLine()));
         }
+        js.sort((p1, p2) ->
+        {
+            if(p1.x == p2.x) return p2.y-p1.y;
+            else return p1.x-p2.x;
+        });
+        Collections.sort(bags);
 
+        int idx = 0;
+
+        for(int b : bags){
+            while(idx < N && b >= js.get(idx).x){
+                jewels.add(js.get(idx).y);
+                idx++;
+            }
+            if(!jewels.isEmpty()){
+                ans += jewels.poll();
+            }
+        }
+        System.out.println(ans);
     }
 }
-class Pair implements Comparable<Pair>{//무게, 가격
+class Pair{//무게, 가격
     int x, y;
     Pair(int x, int y){
         this.x = x;
         this.y = y;
-    }
-    @Override
-    public int compareTo(Pair o) {
-        if(this.y < o.y) return -1;
-        else if(this.y > o.y) return 1;
-        else return Integer.compare(this.x, o.x);
     }
 }
 
