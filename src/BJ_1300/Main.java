97178282e16d786b96e59143f3d867b6d4351369
vattis <andrew0726@naver.com>
2024-07-25 17:14:58 +0900

    .

---
+++ src/BJ_1300/Main.java
@@ -0,0 +1,32 @@
+package BJ_1300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
+class Solution{
+    int N, K;
+    Map<Long, Long> map = new HashMap<>();
+    public Solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+
+    }
+    public Long countLessNum(Long num){
+
+    }
+
+}
+
