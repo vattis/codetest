d4e40f111467f45ae5aee69a4b2b5b7cb95f15f1
vattis <andrew0726@naver.com>
2024-07-26 17:06:34 +0900

    .

---
+++ src/BJ_1562/Main.java
@@ -0,0 +1,25 @@
+package BJ_1562;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Solution {
+    int N;
+    public Solution(int[][] a, int k, int h, int w) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+
+}
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+    }
+}
+
