834b1312602a33a1cbffb05e330d0651c4b58546
vattis <andrew0726@naver.com>
2024-08-02 13:20:38 +0900

    .

--- src/BJ_10816/Main.java
+++ src/BJ_10816/Main.java
@@ -1,4 +1,10 @@
 package BJ_10816;
+//처음에 생각한대로 이분탐색 or 해시를 이용해서 탐색하는 문제
+//나는 해시맵을 사용했음
+//처음엔 해시맵을 사용했는데도 시간 초과 발생
+//출력이 너무 많아서 그런듯?
+//문자열을 한번에 붙여서 출력하여 시간 줄임
+import org.w3c.dom.ls.LSOutput;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,47 +13,45 @@ import java.util.*;
 
 import static java.lang.Math.min;
 import static java.lang.Math.pow;
-//애초에 시작이 0부터였음;;
-//DP 유형이 많이 부족하다고 느낌
-//비트마스크는 할 만 한듯?
-//N이 작을 때 사용하면 효과적
+
 class Solution {
     int N, M;
-    Set<Integer> n = new HashSet<>();
-    List<Integer> m = new ArrayList<>();
+    Map<Integer, Integer> n = new HashMap<>();
+    int[] m = new int[500001];
+    StringBuilder ans = new StringBuilder();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public void Solution() throws IOException {
+    public Solution() throws IOException {
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i = 0; i < N; i++) {
-            n.add(Integer.parseInt(st.nextToken()));
+            int t = Integer.parseInt(st.nextToken());
+            if(n.containsKey(t)){
+                n.replace(t, n.get(t)+1);
+            }else{
+                n.put(t, 1);
+            }
         }
         M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
-            m.add(Integer.parseInt(st.nextToken()));
+            m[i] = Integer.parseInt(st.nextToken());
         }
+        for(int i = 0; i < M; i++){
+            if(n.containsKey(m[i])){
+                ans.append(n.get(m[i])+" ");
+            }
+            else{
+                ans.append(0 + " ");
+            }
+        }
+        System.out.println(ans);
     }
 }
 
 public class Main {
     public static void main(String[] ars) throws IOException {
-        int N;
-        int[][] W = new int[16][];
-        for(int i = 0; i < 16; i++){
-            W[i] = new int[16];
-        }
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int j = 0;
-            while(st.hasMoreTokens()){
-                W[i][j] = Integer.parseInt(st.nextToken());
-                j++;
-            }
-        }
-        Solution solution = new Solution(N, W);
-        System.out.println(solution.solution());
+        Solution sol = new Solution();
     }
 }
 
