f1953f55abf7f358213f335b991900d599fc92c0
vattis <andrew0726@naver.com>
2024-09-19 17:12:10 +0900

    .

---
+++ src/BJ_1039/Main.java
@@ -0,0 +1,26 @@
+package BJ_1039;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] ars) {
+
+    }
+}
+class Solution{
+    int N, K;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public Solution() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+
+    }
+
+}
---
+++ src/BJ_1256/Main.java
@@ -0,0 +1,44 @@
+package BJ_1256;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+        Solution s = new Solution();
+    }
+}
+class Solution{
+    int N, M, K;
+    int count = 0;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder ans = new StringBuilder();
+
+    public Solution() throws IOException {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        ans.append("a".repeat(Math.max(0, N)));
+        ans.append("z".repeat(Math.max(0, M)));
+
+    }
+    String func(int start, int n, int m){
+        ans.setCharAt(start, 'z');
+        ans.setCharAt(start+1, 'a');
+        makeDefault()
+        func(start+1, 1, m-1);
+    }
+    String makeDefault(int n, int m){
+        String str = "";
+        ans.append("a".repeat(Math.max(0, n)));
+        ans.append("z".repeat(Math.max(0, m)));
+        return str;
+    }
+}
+
+
+
