5d4713d10833d26fd6014935821724db026d7d8c
vattis <superhsk00@gmail.com>
2024-09-23 00:44:16 +0900

    .

---
+++ src/BJ_1918/Main.java
@@ -0,0 +1,26 @@
+package BJ_1918;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+        Solution solution = new Solution();
+    }
+}
+class Solution {
+    Stack<Character> operators = new Stack<>();
+    Queue<Character> numbers = new LinkedList<>();
+    public Solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
+        char[] arr = br.readLine().toCharArray();
+        int index = 0;
+
+
+
+    }
+}
+
