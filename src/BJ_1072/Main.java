6007150f44d63de4fed04f676ae1aa8540759c77
vattis <andrew0726@naver.com>
2024-11-28 17:14:13 +0900

    .

---
+++ src/BJ_1072/Main.java
@@ -0,0 +1,30 @@
+package BJ_1072;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+        Solution s = new Solution();
+    }
+}
+class Solution{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public Solution() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+    }
+
+    public int recur(int num, int r, int c){
+
+    }
+}
+
+
+
+
