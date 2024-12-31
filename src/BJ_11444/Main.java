7df70758f418829fd416fffad8b87ee1130749b2
vattis <andrew0726@naver.com>
2024-12-31 17:03:13 +0900

    .

---
+++ src/BJ_11444/Main.java
@@ -0,0 +1,25 @@
+package BJ_11444;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Solution {
+    String N;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public Solution() throws IOException {
+        N = br.readLine();
+    }
+
+}
+
+public class Main {
+    public static void main(String[] ars) throws IOException {
+        Solution sol = new Solution();
+    }
+}
+
